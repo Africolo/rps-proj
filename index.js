9c45dae8b5b2e3ef93d0762652f79568b0d95b92
Africolo <aurasphere6969@gmail.com>
2024-05-24 18:08:50 +0300

    Test 2

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
 console.log("Hello!");
 
-function getComputerChoice() {
-  let num = Math.random();
-  if (num <= 0.3 && num < 0.3) {
-    let object = "Rock";
-    console.log(`Cpu chose ${object}`);
-  } else if (num <= 0.6 && num > 0.3) {
-    let object = "Scissors";
-    console.log(`Cpu chose ${object}`);
-  } else if (num > 0.6) {
-    let object = "Paper";
-    console.log(`Cpu chose ${object}`);
-  }
-}
-console.log(getComputerChoice());
-
 function getHumanChoice() {
-  let human = prompt("Choose an object:  ");
-  human.toUpperCase;
-  console.log(`You chose ${human}`);
+  let humanObj = prompt(`Please enter in an object`).toLowerCase();
+  if (humanObj === "rock" || humanObj === "paper" || humanObj === "scissors") {
+    console.log(`You chose: ${humanObj}`);
+    console.log("Waiting for computers turn...");
+  } else {
+    alert(`Please enter in a correct value.`);
+    getHumanChoice();
+  }
+  return humanObj;
 }
-console.log(getHumanChoice());
+
+function getComputerChoice() {
+  let cpuObj = Math.floor(Math.random() * 3);
+  if (cpuObj === 1) {
+    let object = "rock";
+    console.log(`Cpu chose: ${object}`);
+    alert("*********************");
+    alert("Analyzing Data...");
+  } else if (cpuObj === 2) {
+    let object = "paper";
+    console.log(`Cpu chose: ${object}`);
+    alert("*********************");
+    alert("Analyzing Data...");
+  } else if (cpuObj === 1) {
+    let object = "scissors";
+    console.log(`Cpu chose: ${object}`);
+    console.log("*********************");
+    alert("Analyzing Data...");
+  }
+  return cpuObj;
+}
+let humanScore = 0;
+let cpuScore = 0;
+function playRound(humanChoice, cpuChoice) {
+  if (humanChoice == cpuChoice) {
+    console.log("It's a tie!");
+    alert(`Cpu score: ${cpuScore} Your score: ${humanScore}`);
+  } else if (
+    (humanChoice === "rock" && cpuChoice === "paper") ||
+    (humanChoice === "paper" && cpuChoice === "scissors") ||
+    (humanChoice === "scissors" && cpuChoice === "rock")
+  ) {
+    cpuScore++;
+    console.log(`You lose!`);
+    alert(`Cpu score: ${cpuScore} Your score: ${humanScore}`);
+  } else if (
+    (humanChoice === "paper" && cpuChoice === "rock") ||
+    (humanChoice === "scissors" && cpuChoice === "paper") ||
+    (humanChoice === "rock" && cpuChoice === "scissors")
+  ) {
+    humanScore++;
+    console.log("You win!");
+    alert(`Cpu score: ${cpuScore} Your score: ${humanScore}`);
+  }
+  return humanChoice, cpuChoice;
+}
+const humanSelector = getHumanChoice();
+const cpuSelector = getComputerChoice();
+playRound(humanSelector, cpuSelector);
